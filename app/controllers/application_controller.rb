b52fe67dd8e7e0830b3b25fb9b3a2fd94885a9b7
Pedro Ourique <pedro.g.ourique@gmail.com>
2025-02-24 18:15:15 -0300

    login progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  before_action :authenticate_user!
+
+  helper_method :current_user
+
+  private
+
+  def current_user
+    if session[:user_id]
+      @current_user = User.find_by(id: session[:user_id])
+    elsif
+      @current_user = nil
+    end
+  end
+
+  def authenticate_user!
+    if current_user.nil?
+      redirect_to sign_in_path, alert: "You need to sign in first."
+    end
+  end
 end
